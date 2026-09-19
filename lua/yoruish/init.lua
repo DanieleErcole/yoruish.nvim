7df13d5b96a801f21e9f889e0a58dc91708b3b9c
DanieleErcole <danieleercole14@protonmail.com>
2026-09-19 13:22:11 +0200

    property color

--- lua/yoruish/init.lua
+++ lua/yoruish/init.lua
@@ -167,7 +167,7 @@ local set_groups = function()
         ["@keyword.function"] = { fg = c.cyan, style = cfg.function_style },
         ["@namespace"] = { fg = c.cyan, italic = true },
         ["@parameter"] = { fg = c.magenta },
-        ["@property"] = { fg = c.gray07 },
+        ["@property"] = { fg = c.bright_blue },
         ["@punctuation"] = { fg = c.gray06 },
         ["@punctuation.delimiter"] = { fg = c.gray06 },
         ["@punctuation.bracket"] = { fg = c.gray06 },
